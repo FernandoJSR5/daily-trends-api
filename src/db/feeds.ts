ef45cee2ca2317780532598127fffb0192a8ce14
fsalazar5 <fsalazarr@ripley.com>
2023-12-20 15:14:59 +0100

    added feed schema data with mongoose ODM

--- src/db/feeds.ts
+++ src/db/feeds.ts
@@ -1,6 +1,34 @@
 import mongoose from 'mongoose';
 
-const FeedSchema = new mongoose.Schema({});
+const FeedSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    author: {
+      type: String,
+      required: true,
+    },
+    journal: {
+      type: String,
+      required: true,
+    },
+    link: {
+      type: String,
+      required: true,
+    },
+    image_link: {
+      data: Buffer,
+      contentType: String,
+    },
+  },
+  { timestamps: true }
+);
 
 export const FeedModel = mongoose.model('Feed', FeedSchema);
 export const getFeeds = () => FeedModel.find();
